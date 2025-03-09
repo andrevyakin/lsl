25d34c16be4f372df3322c0ca21745933a37db1f
andrevyakin <andrevyakin@gmail.com>
2025-03-10 00:32:06 +0300

    the production component is started

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,3 @@
-import logo from '../assets/lskNoBg.png'
-
 const Navigation = () => {
 
     return (
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import products from '../constants/products.json';
 
 const Products = () => {
+
     return (
-        <div id="products" className="h-[1000px] bg-amber-100">Products</div>
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div
+                    className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                    {products.map((product) => (
+                        <div key={product.id} className="group">
+                            <img
+                                alt={product.name}
+                                src={`products/${product.imageSrc[0]}.png`}
+                                className="aspect-square w-full rounded-lg bg-gray-200 object-contain group-hover:opacity-75 xl:aspect-7/8"
+                            />
+                            <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                            <p>{product.description}</p>
+                            <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 export default Products
